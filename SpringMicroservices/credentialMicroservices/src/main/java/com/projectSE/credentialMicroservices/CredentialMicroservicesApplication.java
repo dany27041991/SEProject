bb2f94d133bfca89b62d88022c49cbb3368963f3
Danilo Giovannico <dany27041991@gmail.com>
2018-08-20 17:00:09 +0200

    First Microservices

---
+++ SpringMicroservices/credentialMicroservices/src/main/java/com/projectSE/credentialMicroservices/CredentialMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.projectSE.credentialMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialMicroservicesApplication.class, args);
+	}
+}
