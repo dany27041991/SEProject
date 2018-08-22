e9edb5229993ca94ebedb66ce0239a287106335a
Danilo Giovannico <dany27041991@gmail.com>
2018-08-22 18:46:39 +0200

    footer

--- AngularSide/AngularSE/src/app/app.module.ts
+++ AngularSide/AngularSE/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {HttpClientModule} from '@angular/common/http';
+import {RoutingModuleModule} from './routing-module.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    AngularFontAwesomeModule,
+    HttpClientModule,
+    RoutingModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularSide/AngularSE/src/app/routing-module.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: ''
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class RoutingModuleModule { }
