4509888a19638d9fd10680d0f8ff6dc57bf96919
Danilo Giovannico <dany27041991@gmail.com>
2019-01-25 14:27:10 +0100

    Logout

--- AngularSide/AngularSE/src/app/services/auth.service.ts
+++ AngularSide/AngularSE/src/app/services/auth.service.ts
@@ -2,11 +2,13 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Cookie} from 'ng2-cookies/ng2-cookies';
 import { Type} from '../utils/type';
+import {ResponseInterface} from '../interfaces/ResponseInterface';
 
 @Injectable()
 export class AuthService {
   private isUserLogged = false;
   private APIAUTHURL = 'http://localhost:8090/';
+  private typeOfUser: number;
   constructor(private http: HttpClient) {}
 
   isUserLoggedIn() {}
@@ -20,7 +22,9 @@ export class AuthService {
       (payload) => {
         const token: string = payload.headers.get('Authorization');
         Cookie.set('Jwt', token);
-        const personType: number = payload.body['response'].person_type;
+        const personBody =  <ResponseInterface>payload.body;
+        const personType: number = personBody.response['person_type'];
+        this.typeOfUser = personType;
         switch (personType) {
           case Type.Secretary: {
             // console.log('Secretary')
@@ -73,7 +77,31 @@ export class AuthService {
     );
   }
   signUp() {}
-  logout() {}
+  logout() {
+    Cookie.delete('Jwt');
+    switch (this.typeOfUser) {
+      case Type.Secretary: {
+        localStorage.removeItem('Secretary');
+        this.isUserLogged = false;
+        break;
+      }
+      case Type.Professor: {
+        localStorage.removeItem('Professor');
+        this.isUserLogged = false;
+        break;
+      }
+      case Type.Student: {
+        localStorage.removeItem('Student');
+        this.isUserLogged = false;
+        break;
+      }
+      default: {
+        // console.log('Default')
+        break;
+      }
+    }
+    return true;
+  }
   getUser(): Object {
     const data = JSON.parse(localStorage.getItem('person'));
     return null;
